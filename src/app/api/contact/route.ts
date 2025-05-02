d15bb77abc2df2ef4d53df5f4b07ea87e1ea6a24
Kaminskyi22 <kaminskyi.vlad@icloud.com>
2025-05-02 17:50:00 +0300

    TELEGRAM_CHAT_ID тепер зчитується зі змінної середовища

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 
 const TELEGRAM_BOT_TOKEN = '7710869128:AAEGhP3Q3Ee0PFoqdhT-US0ITXDZPjUHM0w';
-const TELEGRAM_CHAT_ID = '6125664936'; // Замініть на ваш chat_id
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID!;
 
 export async function POST(request: Request) {
   try {
